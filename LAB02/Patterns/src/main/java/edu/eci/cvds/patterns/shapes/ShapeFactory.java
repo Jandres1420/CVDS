1eebbaf8afc62221facddb10af6184f499492b69
Jandres1420 <juan.pico-m@mail.escuelaing.edu.co>
2020-08-18 18:52:22 -0500

    Laboratorio 2 completado

---
+++ LAB02/Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -0,0 +1,22 @@
+
+package edu.eci.cvds.patterns.shapes;
+
+import edu.eci.cvds.patterns.shapes.concrete.*;
+
+public class ShapeFactory {
+    public static Shape create(RegularShapeType type) {
+        switch(type){
+        	case Triangle:
+        		return new Triangle();
+        		
+        	case Pentagon:
+        		return new Pentagon();
+        	case Quadrilateral:
+        		return new Quadrilateral();
+        	case Hexagon:
+        		return new Hexagon();
+        	default : 
+        		return null;
+        }
+    }
+}
